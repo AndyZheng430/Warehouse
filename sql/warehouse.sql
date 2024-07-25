09307e46c77b8c2e3d52cd589fd81083a42645e7
AndyZheng430 <andy.zheng.430@stonybrook.edu>
2024-07-25 18:00:27 -0400

    added sql table creation script

---
+++ sql/warehouse.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS Warehouse (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    location VARCHAR(255),
+    max_capacity BIGINT,
+    owner VARCHAR(255)
+);
+
+CREATE TABLE Storage (
+    id BIGSERIAL PRIMARY KEY,
+    amount BIGINT,
+    item_id references Item(id),
+    warehouse_id references Warehouse(id)
+);
+
+CREATE TABLE Item (
+    id BIGSERIAL,
+    name VARCHAR(255),
+    description Text
+);
+
+CREATE TABLE ItemFormula (
+    id BIGSERIAL,
+    material VARCHAR(255),
+    amount int,
+    item_id references Item(id)
+);
