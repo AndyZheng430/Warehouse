95a36220339135eb349bdea72f091686250b8353
Melwinj <mjames24@asu.edu>
2024-08-30 10:55:06 -0700

    DB init on backend startup

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+--1. Clear existing data using TRUNCATE with CASCADE
+TRUNCATE TABLE inventory;
+TRUNCATE TABLE item RESTART IDENTITY CASCADE;
+TRUNCATE TABLE warehouse RESTART IDENTITY CASCADE;
+
+
+-- 2. Insert mock data into warehouse table
+INSERT INTO warehouse (name, location, owner, max_capacity) VALUES
+('Woodshop', 'New York', 'John Doe', 1000),
+('Clothing depot', 'Los Angeles', 'Jane Smith', 1500),
+('Water works', 'Chicago', 'Alice Johnson', 1200);
+
+-- 3. Insert mock data into item table
+INSERT INTO item (name, description) VALUES
+('Oak Wood', 'Premium wood'),
+('Couch', 'Furniture Item'),
+('Shirt', 'Clothing Item');
+
+-- 4. Insert mock data into inventory table
+-- Ensure that the warehouse_id and item_id match the IDs inserted into the warehouse and item tables
+INSERT INTO inventory (warehouse_id, item_id, amount) VALUES
+(1, 1, 100),
+(1, 2, 200),
+(2, 1, 150),
+(2, 3, 50),
+(3, 2, 300),
+(3, 3, 250);
